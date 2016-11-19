e71aec12dd5e7539a2c3c5fe267e75b340b2d2fb
duckish <nguyen_vanduc@hotmail.com>
2016-11-19 18:38:44 -0500

    implementation de Swagger

--- src/apimetaclub/urls.py
+++ src/apimetaclub/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
+# Inlcude the schema view in our urls.
 urlpatterns = [
+    url(r'^', schema_view, name="docs"),
+    url(r'^users/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
 ]
